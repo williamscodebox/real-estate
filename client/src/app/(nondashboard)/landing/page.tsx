3c8f1f018bac1971f243a61d3c93d465d694c129
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-11 00:03:18 -0400

    created FooterSection and FeaturesSection components

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import FooterSection from "./FooterSection";
+import FeaturesSection from "./FeaturesSection";
 
 function Landing() {
   return (
     <div>
       <HeroSection />
+      <FeaturesSection />
+      {/*<FooterSection />*/}
     </div>
   );
 }
