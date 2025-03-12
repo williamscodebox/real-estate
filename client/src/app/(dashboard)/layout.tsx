ed7d2bfb76b6ad25d61835781d7bcf23a9cb81f4
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-12 01:25:21 -0400

    updated dashboard layout

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -1,16 +1,48 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import Sidebar from "@/components/AppSidebar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useGetAuthUserQuery } from "@/state/api";
+import { usePathname, useRouter } from "next/navigation";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === "manager" && pathname.startsWith("/tenants")) ||
+        (userRole === "tenant" && pathname.startsWith("/managers"))
+      ) {
+        router.push(
+          userRole === "manager"
+            ? "/managers/properties"
+            : "/tenants/favorites",
+          { scroll: false }
+        );
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, router, pathname]);
+
+  if (authLoading || isLoading) return <>Loading...</>;
+  if (!authUser?.userRole) return null;
+
   return (
     <SidebarProvider>
       <div className="min-h-screen w-full bg-primary-100">
         <Navbar />
         <div style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
           <main className="flex">
-            <Sidebar />
+            <Sidebar userType={authUser.userRole.toLowerCase()} />
             <div className="flex-grow transition-all duration-300">
               {children}
             </div>
