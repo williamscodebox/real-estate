ac4bd183fb2d097b86303636a749db2cbc9aa45a
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-12 17:08:35 -0400

    updated tenant settings page

--- client/src/app/(dashboard)/tenants/settings/page.tsx
+++ client/src/app/(dashboard)/tenants/settings/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import {
+  useGetAuthUserQuery,
+  useUpdateTenantSettingsMutation,
+} from "@/state/api";
+import SettingsForm from "@/components/SettingsForm";
 import React from "react";
 
 function TenantSettings() {
-  return <div>TenantSettings</div>;
+  const { data: authUser, isLoading } = useGetAuthUserQuery();
+  const [updateTenant] = useUpdateTenantSettingsMutation();
+
+  if (isLoading) return <>Loading...</>;
+
+  const initialData = {
+    name: authUser?.userInfo.name,
+    email: authUser?.userInfo.email,
+    phoneNumber: authUser?.userInfo.phoneNumber,
+  };
+
+  const handleSubmit = async (data: typeof initialData) => {
+    await updateTenant({
+      cognitoId: authUser?.cognitoInfo?.userId,
+      ...data,
+    });
+  };
+  return (
+    <SettingsForm
+      initialData={initialData}
+      onSubmit={handleSubmit}
+      userType="tenant"
+    />
+  );
 }
 
 export default TenantSettings;
