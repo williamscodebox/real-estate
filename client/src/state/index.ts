f7cae121aea009d2341fa0f5d38dd8092a10913d
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-14 13:51:43 -0400

    updated global state

--- client/src/state/index.ts
+++ client/src/state/index.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface FiltersState {
   location: string;
@@ -12,14 +12,45 @@ export interface FiltersState {
   coordinates: [number, number];
 }
 
-export const initialState = {};
+interface InitialStateTypes {
+  isFiltersFullOpen: boolean;
+  filters: FiltersState;
+  viewmode: "grid" | "list";
+}
+
+export const initialState: InitialStateTypes = {
+  isFiltersFullOpen: false,
+  filters: {
+    location: "Los Angeles",
+    beds: "any",
+    baths: "any",
+    propertyType: "any",
+    amenities: [],
+    availableFrom: "any",
+    priceRange: [null, null],
+    squareFeet: [null, null],
+    coordinates: [34.0522, -118.2437],
+  },
+  viewmode: "grid",
+};
 
 export const globalSlice = createSlice({
   name: "global",
   initialState,
-  reducers: {},
+  reducers: {
+    setFilters: (state, action: PayloadAction<Partial<FiltersState>>) => {
+      state.filters = { ...state.filters, ...action.payload };
+    },
+    setViewMode: (state, action: PayloadAction<"grid" | "list">) => {
+      state.viewmode = action.payload;
+    },
+    toggleFiltersFullOpen: (state) => {
+      state.isFiltersFullOpen = !state.isFiltersFullOpen;
+    },
+  },
 });
 
-export const {} = globalSlice.actions;
+export const { setFilters, toggleFiltersFullOpen, setViewMode } =
+  globalSlice.actions;
 
 export default globalSlice.reducer;
