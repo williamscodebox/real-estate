6e3d9f5f75f57a15f2db7f0976921fc8a1e0396e
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-13 22:55:58 -0400

    updated tenantControllers

--- server/src/controllers/tenantControllers.ts
+++ server/src/controllers/tenantControllers.ts
@@ -118,3 +118,69 @@ export const getCurrentResidences = async (
       .json({ message: `Error retrieving manager properties: ${err.message}` });
   }
 };
+
+export const addFavoriteProperty = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { cognitoId, propertyId } = req.params;
+    const tenant = await prisma.tenant.findUnique({
+      where: { cognitoId },
+      include: { favorites: true },
+    });
+
+    if (!tenant) {
+      res.status(404).json({ message: "Tenant not found" });
+      return;
+    }
+
+    const propertyIdNumber = Number(propertyId);
+    const existingFavorites = tenant.favorites || [];
+
+    if (!existingFavorites.some((fav) => fav.id === propertyIdNumber)) {
+      const updatedTenant = await prisma.tenant.update({
+        where: { cognitoId },
+        data: {
+          favorites: {
+            connect: { id: propertyIdNumber },
+          },
+        },
+        include: { favorites: true },
+      });
+      res.json(updatedTenant);
+    } else {
+      res.status(409).json({ message: "Property already added as favorite" });
+    }
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error adding favorite property: ${error.message}` });
+  }
+};
+
+export const removeFavoriteProperty = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { cognitoId, propertyId } = req.params;
+    const propertyIdNumber = Number(propertyId);
+
+    const updatedTenant = await prisma.tenant.update({
+      where: { cognitoId },
+      data: {
+        favorites: {
+          disconnect: { id: propertyIdNumber },
+        },
+      },
+      include: { favorites: true },
+    });
+
+    res.json(updatedTenant);
+  } catch (err: any) {
+    res
+      .status(500)
+      .json({ message: `Error removing favorite property: ${err.message}` });
+  }
+};
