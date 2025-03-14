fc93d732fe76bb0ebf5b301a3caf594cc8cc1440
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-13 22:57:44 -0400

    updated tenantRoutes

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -4,8 +4,8 @@ import {
   createTenant,
   updateTenant,
   getCurrentResidences,
-  //addFavoriteProperty,
-  //removeFavoriteProperty,
+  addFavoriteProperty,
+  removeFavoriteProperty,
 } from "../controllers/tenantControllers";
 
 const router = express.Router();
@@ -14,5 +14,7 @@ router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant);
 router.post("/", createTenant);
 router.get("/:cognitoId/current-residences", getCurrentResidences);
+router.post("/:cognitoId/favorites/:propertyId", addFavoriteProperty);
+router.delete("/:cognitoId/favorites/:propertyId", removeFavoriteProperty);
 
 export default router;
