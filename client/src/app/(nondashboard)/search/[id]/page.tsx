331d08eab17c52f0ddeca289df4b92bdd080c97d
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-16 16:03:41 -0400

    updated single listing page and added ImagePreviews component

--- client/src/app/(nondashboard)/search/[id]/page.tsx
+++ client/src/app/(nondashboard)/search/[id]/page.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
+
+import { useGetAuthUserQuery } from "@/state/api";
+import { useParams } from "next/navigation";
+import React, { useState } from "react";
+import ImagePreviews from "./imagePreviews";
 
 function SingleListing() {
-  return <div>SingleListing</div>;
+  const { id } = useParams();
+  const propertyId = Number(id);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data: authUser } = useGetAuthUserQuery();
+
+  return (
+    <div>
+      <ImagePreviews
+        images={["/singlelisting-2.jpg", "/singlelisting-3.jpg"]}
+      />
+      <div className="flex flex-col md:flex-row justify-center gap-10 mx-10 md:w-2/3 md:mx-auto mt-16 mb-8">
+        <div className="order-2 md:order-1">
+          {/*<PropertyOverview propertyId={propertyId} />
+          <PropertyDetails propertyId={propertyId} />
+          <PropertyLocation propertyId={propertyId} />*/}
+        </div>
+
+        <div className="order-1 md:order-2">
+          {/*<ContactWidget onOpenModal={() => setIsModalOpen(true)} />*/}
+        </div>
+      </div>{" "}
+      {/*
+      {authUser && (
+        <ApplicationModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          propertyId={propertyId}
+        />
+      )}*/}
+    </div>
+  );
 }
 
 export default SingleListing;
