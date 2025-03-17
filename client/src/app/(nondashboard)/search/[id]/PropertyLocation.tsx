3775ad497df302e76e7398721cdd4e45cf264734
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-16 21:59:46 -0400

    updated PropertyLocation component

--- client/src/app/(nondashboard)/search/[id]/PropertyLocation.tsx
+++ client/src/app/(nondashboard)/search/[id]/PropertyLocation.tsx
@@ -19,7 +19,7 @@ function PropertyLocation({ propertyId }: PropertyDetailsProps) {
 
     const map = new mapboxgl.Map({
       container: mapContainerRef.current!,
-      style: "mapbox://styles/majesticglue/cm6u301pq008b01sl7yk1cnvb",
+      style: "mapbox://styles/willproject/cm89ecp3l00m301s5161z3svh",
       center: [
         property.location.coordinates.longitude,
         property.location.coordinates.latitude,
