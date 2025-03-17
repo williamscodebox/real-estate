b988f1d9c58b5e22524c6b2d60e5d8cfc5a1dad5
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-17 02:50:19 -0400

    updated add property page

--- client/src/app/(dashboard)/managers/newproperty/page.tsx
+++ client/src/app/(dashboard)/managers/newproperty/page.tsx
@@ -10,10 +10,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 function NewProperty() {
   const [createProperty] = useCreatePropertyMutation();
   const { data: authUser } = useGetAuthUserQuery();
+  const router = useRouter();
 
   const form = useForm<PropertyFormData>({
     resolver: zodResolver(propertySchema),
@@ -61,6 +63,7 @@ function NewProperty() {
     formData.append("managerCognitoId", authUser.cognitoInfo.userId);
 
     await createProperty(formData);
+    router.push(`/managers/properties`);
   };
 
   return (
