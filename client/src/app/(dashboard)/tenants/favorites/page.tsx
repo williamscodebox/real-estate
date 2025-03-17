d2ad00a115d41dc940a4d81095626e7547a3d019
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-16 22:35:02 -0400

    added Header and Loading components

--- client/src/app/(dashboard)/tenants/favorites/page.tsx
+++ client/src/app/(dashboard)/tenants/favorites/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import Card from "@/components/Card";
+import Header from "@/components/Header";
+import Loading from "@/components/Loading";
+import {
+  useGetAuthUserQuery,
+  useGetPropertiesQuery,
+  useGetTenantQuery,
+} from "@/state/api";
 import React from "react";
 
 function Favorites() {
